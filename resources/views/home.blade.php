3404fdca33c3a5b5dba020e10546d179627e726d
Raxmatilla97 <wi.fi.xor@gmail.com>
2022-12-22 11:35:59 +0500

    Dashbordga yangi widgetlar qo'shdim

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,72 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Boshqaruv paneliga Xush kelibsiz!</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Asosiy sahifa</a></li>
+                        <li class="breadcrumb-item active">Boshqaruv paneli</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
+    <div class="container">
+        <div class="row">
+           <div class="col-lg-3 col-6">
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3>{{ $umumiySon }} <sup style="font-size: 20px">- ta</sup></h3>
+                        <p>{{"Ro'yxatdan o'tgan Clientlar umumiy soni"}}</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-person-add"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">Sahifaga o'tish<i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3>{{ $keganClient }} <sup style="font-size: 20px">- ta</sup></h3>
+                        <p>{{"O'quv markaziga kelgan Clientlar"}}</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">Sahifaga o'tish<i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-danger">
+                    <div class="inner">
+                        <h3>{{ $kelmaganClient }} <sup style="font-size: 20px">- ta</sup></h3>
+                        <p>{{"O'quv markaziga kelmagan Clientlar"}}</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-pie-graph"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">Sahifaga o'tish <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-6">
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3>150 <sup style="font-size: 20px">- ta</sup></h3>
+                        <p>{{"Qayta qo'ng'iroq qilinganlar"}}</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-bag"></i>
+                    </div>
+                    <a href="#" class="small-box-footer">Sahifaga o'tish <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
