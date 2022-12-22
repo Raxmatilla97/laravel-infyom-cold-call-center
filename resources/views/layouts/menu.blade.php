8de43a48579113ccbe371c43eee5597820f46176
Raxmatilla97 <wi.fi.xor@gmail.com>
2022-12-22 11:36:15 +0500

    Menyularni tog'riladim

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,15 +1,16 @@
+
 <li class="nav-item">
-    <a href="{{ route('categories.index') }}"
-       class="nav-link {{ Request::is('categories*') ? 'active' : '' }}">
-        <p>Categories</p>
+    <a href="{{ route('qongiroqQilganlars.index') }}"
+       class="nav-link {{ Request::is('qongiroqQilganlars*') ? 'active' : '' }}">
+        <p>Qo'ng'iroq qilganlarning umumiy ro'yxati</p>
     </a>
 </li>
 
 
 <li class="nav-item">
-    <a href="{{ route('qongiroqQilganlars.index') }}"
-       class="nav-link {{ Request::is('qongiroqQilganlars*') ? 'active' : '' }}">
-        <p>Qongiroq Qilganlars</p>
+    <a href="{{ route('categories.index') }}"
+       class="nav-link {{ Request::is('categories*') ? 'active' : '' }}">
+        <p>O'quv yo'nalishlari</p>
     </a>
 </li>
 
