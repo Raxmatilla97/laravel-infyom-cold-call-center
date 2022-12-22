04010b7d63ed2ccb76e0228ee3c9c357a977cf8f
Raxmatilla97 <wi.fi.xor@gmail.com>
2022-12-22 11:33:54 +0500

    Home dashbordni tog'irladim

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\QongiroqQilganlar;
+
 class HomeController extends Controller
 {
     /**
@@ -23,6 +25,13 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $umumiySon = QongiroqQilganlar::get()->count();
+        $keganClient = QongiroqQilganlar::where('keldimi', 1)->get()->count();
+        $kelmaganClient = QongiroqQilganlar::where('keldimi', 0)->get()->count();
+        
+        return view('home')
+        ->with('umumiySon', $umumiySon)
+        ->with('keganClient', $keganClient)
+        ->with('kelmaganClient', $kelmaganClient);
     }
 }
