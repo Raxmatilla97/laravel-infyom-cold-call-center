2a3732962340059fdfb5634ab0d937e3a91d8e15
Raxmatilla97 <wi.fi.xor@gmail.com>
2022-12-22 11:37:55 +0500

    Viewsda show blade ni tarjima qildim

--- resources/views/qongiroq_qilganlars/show.blade.php
+++ resources/views/qongiroq_qilganlars/show.blade.php
@@ -5,12 +5,12 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Qongiroq Qilganlar Details</h1>
+                    <h1>Clientning ma'lumotlarini to'liq ko'rish</h1>
                 </div>
                 <div class="col-sm-6">
                     <a class="btn btn-default float-right"
                        href="{{ route('qongiroqQilganlars.index') }}">
-                        Back
+                        Orqaga qaytish
                     </a>
                 </div>
             </div>
