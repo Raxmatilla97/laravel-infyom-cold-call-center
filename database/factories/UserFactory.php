fee34c8f464f1a55e0b70d43a23ae4390d249fe4
Raxmatilla97 <wi.fi.xor@gmail.com>
2022-12-23 16:29:13 +0500

    User faktoryni tog'irladim

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,10 +15,10 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => "Raxmatilla",
+            'email' => "wi.fi.xor@gmail.com",
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$XhMcg/Y3f3VtSBKgqCjySuit5ZVvplhSLjxVpBwoauH3zAEIDueHC', // 5579187**
             'remember_token' => Str::random(10),
         ];
     }
