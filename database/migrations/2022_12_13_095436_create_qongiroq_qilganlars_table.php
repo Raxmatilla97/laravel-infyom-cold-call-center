65f4570c201e30ddc3c89cc6d2a6b11080bee94e
Raxmatilla97 <wi.fi.xor@gmail.com>
2022-12-23 16:29:33 +0500

    Migratsiyani sozladim

--- database/migrations/2022_12_13_095436_create_qongiroq_qilganlars_table.php
+++ database/migrations/2022_12_13_095436_create_qongiroq_qilganlars_table.php
@@ -23,12 +23,12 @@ class CreateQongiroqQilganlarsTable extends Migration
             $table->double('telefon_nomer');
             $table->double('parent_telefon');
             $table->string('manzil');
-            $table->boolean('qayta_tell_qilindimi');
-            $table->date('qayta_tell_qilingan_sana');
+            $table->boolean('qayta_tell_qilindimi')->nullable();
+            $table->date('qayta_tell_qilingan_sana')->nullable();
             $table->unsignedBigInteger('oquv_yonalishi');
-            $table->boolean('keldimi');
+            $table->boolean('keldimi')->nullable();
             $table->text('qoshimcha_desc');
-            $table->string('recall');
+            $table->string('recall')->nullable();
             $table->date('oquvchi_keladigan_kun');
             $table->foreign('oquv_yonalishi')
               ->references('id')->on('categories')->onDelete('cascade');
