dede2d086ebcfc4f1844a688451c3849b15c0bc3
Raxmatilla97 <wi.fi.xor@gmail.com>
2022-12-23 16:28:59 +0500

    Kichik tog'rilash

--- database/factories/QongiroqQilganlarFactory.php
+++ database/factories/QongiroqQilganlarFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\QongiroqQilganlar;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Categories;
 
 class QongiroqQilganlarFactory extends Factory
 {
@@ -22,19 +23,22 @@ class QongiroqQilganlarFactory extends Factory
     public function definition()
     {
         return [
-            'familya' => $this->faker->word,
-        'ism' => $this->faker->word,
-        'sharif' => $this->faker->word,
-        'tugulgan_kun' => $this->faker->word,
-        'telefon_nomer' => $this->faker->word,
-        'parent_telefon' => $this->faker->word,
-        'manzil' => $this->faker->word,
-        'oquv_yonalishi' => $this->faker->randomDigitNotNull,
-        'qoshimcha_desc' => $this->faker->text,
-        'recall' => $this->faker->word,
-        'oquvchi_keladigan_kun' => $this->faker->word,
-        'created_at' => $this->faker->date('Y-m-d H:i:s'),
-        'updated_at' => $this->faker->date('Y-m-d H:i:s')
+            'familya' => $this->faker->firstName,
+            'ism' => $this->faker->lastName,
+            'sharif' => $this->faker->name,
+            'tugulgan_kun' => $this->faker->date('Y-m-d H:i:s'),
+            'telefon_nomer' => $this->faker->e164PhoneNumber,
+            'parent_telefon' => $this->faker->e164PhoneNumber,
+            'manzil' => $this->faker->streetAddress,
+            'oquv_yonalishi' => $this->faker->randomElement(Categories::pluck('id')),
+            'qoshimcha_desc' => $this->faker->text,
+            'qayta_tell_qilindimi' => $this->faker->randomElement(['0', '1']),
+            'keldimi' => $this->faker->randomElement(['0', '1']),
+            'recall' => $this->faker->text($maxNbChars = 200),
+            'oquvchi_keladigan_kun' => $this->faker->date('Y-m-d H:i:s'),
+            'qayta_tell_qilingan_sana' => $this->faker->date('Y-m-d H:i:s'),
+            'created_at' => $this->faker->date('Y-m-d H:i:s'),
+            'updated_at' => $this->faker->date('Y-m-d H:i:s')
         ];
     }
 }
