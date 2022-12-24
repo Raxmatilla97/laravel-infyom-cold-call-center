c1fc473d0f5b2cf0780669fd19a9c77c9395a54e
Raxmatilla97 <wi.fi.xor@gmail.com>
2022-12-24 14:08:04 +0500

    So'ngi router tog'irlandi

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,30 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
+});
+
+Route::get('/register', function () {
+    return redirect()->route('login');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->group(function () {   
+
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('categories', App\Http\Controllers\CategoriesController::class);
+    Route::resource('qongiroqQilganlars', App\Http\Controllers\QongiroqQilganlarController::class);
 
 
-Route::resource('categories', App\Http\Controllers\CategoriesController::class);
+    Route::get('/kelmaganlar', [App\Http\Controllers\QongiroqQilganlarController::class, 'kelmaganlar_index'])->name('kelmaganlar');
+
+    Route::get('/kelganlar', [App\Http\Controllers\QongiroqQilganlarController::class, 'kelganlar_index'])->name('kelganlar');
+
+    Route::get('/telefon_qilinganlar', [App\Http\Controllers\QongiroqQilganlarController::class, 'telefon_qilinganlar_index'])->name('telefon_qilinganlar');
+
+    Route::get('/telefon_qilinmaganlar', [App\Http\Controllers\QongiroqQilganlarController::class, 'telefon_qilinmaganlar_index'])->name('telefon_qilinmaganlar');
 
 
-Route::resource('qongiroqQilganlars', App\Http\Controllers\QongiroqQilganlarController::class);
+});
