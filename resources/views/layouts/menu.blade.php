ba35f1bad1e7e44adfcce7d28965bfe7f42f0259
Raxmatilla97 <wi.fi.xor@gmail.com>
2022-12-24 14:06:45 +0500

    Menyu qo'shdim

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,9 +2,43 @@
 <li class="nav-item">
     <a href="{{ route('qongiroqQilganlars.index') }}"
        class="nav-link {{ Request::is('qongiroqQilganlars*') ? 'active' : '' }}">
-        <p>Qo'ng'iroq qilganlarning umumiy ro'yxati</p>
+        <p>Umumiy Clientlar ro'yxati</p>
     </a>
 </li>
+
+
+<li class="nav-item">
+   <a href="{{ route('kelganlar') }}"
+      class="nav-link {{ Request::is('kelganlar*') ? 'active' : '' }}">
+      <p>Kelgan Clientlar</p>
+   </a>
+</li>
+
+<li class="nav-item">
+   <a href="{{ route('kelmaganlar') }}"
+      class="nav-link {{ Request::is('kelmaganlar*') ? 'active' : '' }}">
+       <p>Kelmagan Clientlar</p>
+   </a>
+</li>
+
+
+
+<li class="nav-item">
+    <a href="{{ route('telefon_qilinganlar') }}"
+       class="nav-link {{ Request::is('telefon_qilinganlar*') ? 'active' : '' }}">
+        <p>Qayta telefron qilingan Clientlar</p>
+    </a>
+</li>
+
+
+
+<li class="nav-item">
+    <a href="{{ route('telefon_qilinmaganlar') }}"
+       class="nav-link {{ Request::is('telefon_qilinmaganlar*') ? 'active' : '' }}">
+        <p>Qayta telefon qilinmagan Clientlar</p>
+    </a>
+</li>
+
 
 
 <li class="nav-item">
